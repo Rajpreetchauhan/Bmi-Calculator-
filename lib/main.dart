2331589e4215123fb7ca49ba89b1a5c53cd2b0a7
Rajpreetchauhan <rajpreetchauhanofficial@gmail.com>
2023-04-03 12:08:54 +0530

    Showing overflow and unexpected null value

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+
+import 'malefemaleselect.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PlusBox plusBox1 = PlusBox(t: "Male");
+  PlusBox plusBox2 = PlusBox(t: "female");
+
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text("Bmi Calculator")),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Malefemaleselect(
+                      i: Icon(Icons.male),
+                      t: "Male",
+                    ),
+                    Malefemaleselect(
+                      i: Icon(Icons.female),
+                      t: "Female",
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Height"),
+                        Text("177 cm"),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [plusBox1, plusBox2],
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text("Calculate"),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Malefemaleselect extends StatefulWidget {
+  Icon? i;
+  String? t;
+  Malefemaleselect({
+    Key? key,
+    required Icon? i,
+    required String? t,
+  }) : super(key: key);
+
+  @override
+  State<Malefemaleselect> createState() => _MalefemaleselectState();
+}
+
+class _MalefemaleselectState extends State<Malefemaleselect> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.all(Radius.circular(5))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [widget.i!, Text(widget.t!)],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/malefemaleselect.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class PlusBox extends StatefulWidget {
+  const PlusBox({Key? key, required this.t}) : super(key: key);
+  final String t;
+  @override
+  State<PlusBox> createState() => _PlusBoxState();
+}
+
+class _PlusBoxState extends State<PlusBox> {
+  int inum = 0;
+
+  Widget plusMinusButton({required Function()? f, required Widget? i}) {
+    return Container(
+      height: 40,
+      width: 40,
+      decoration: BoxDecoration(
+          color: Colors.red[100], borderRadius: BorderRadius.circular(90)),
+      child: TextButton(onPressed: f, child: i!),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.red, borderRadius: BorderRadius.circular(5)),
+          child: Column(
+            children: [
+              Text(widget.t),
+              Text(inum.toString()),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  plusMinusButton(
+                      i: Icon(Icons.remove),
+                      f: () {
+                        setState(() {});
+                        inum--;
+                      }),
+                  plusMinusButton(
+                      i: Icon(Icons.add),
+                      f: () {
+                        setState(() {});
+                        inum++;
+                      }),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
